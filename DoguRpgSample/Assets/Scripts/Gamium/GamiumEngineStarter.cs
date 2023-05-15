32745e86e15e7605241f98f8d64e07e44fcb2efe
Byoungwook Park <qkrquddnr99@gmail.com>
2023-05-15 22:55:15 +0900

    DoguRpgSample EngineStarter simple way

--- DoguRpgSample/Assets/Scripts/Gamium/GamiumEngineStarter.cs
+++ DoguRpgSample/Assets/Scripts/Gamium/GamiumEngineStarter.cs
@@ -7,41 +7,6 @@ public class GamiumEngineStarter
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void OnLoad()
     {
-        new ServerBuilder()
-            .SetStateHandler(new EventHandler())
-            .SetConfig(new ServerConfig
-            {
-                inputMappings = new InputMapping[]
-                {
-                    new InputMapping()
-                    {
-                        alias = "Horizontal", positiveCodes = new HashSet<KeyCode>() { KeyCode.D },
-                        negativeCodes = new HashSet<KeyCode>() { KeyCode.A }
-                    },
-                    new InputMapping()
-                    {
-                        alias = "Vertical", positiveCodes = new HashSet<KeyCode>() { KeyCode.W },
-                        negativeCodes = new HashSet<KeyCode>() { KeyCode.S }
-                    },
-                },
-                isVerbose = true
-            }).Run();
-    }
-
-    private class EventHandler : Gamium.IEventHandler
-    {
-        private bool isInit;
-
-        public void OnAccept(EventContext eventContext)
-        {
-        }
-
-        public void Update()
-        {
-        }
-
-        public void OnClose(EventContext eventContext)
-        {
-        }
+        new ServerBuilder().Run();
     }
 }
